9f074a590c822b4f8d7cdecf77d8ed9ad98c2ad8
martin2387 <cheenote@hotmail.com>
2013-08-14 11:31:42 -0400

    Whateva!!! arty is lame!!!

--- src/InsurancePremium.java
+++ src/InsurancePremium.java
@@ -1,23 +1,27 @@
-// -------INSTRUCTIONS-------
-// 1. Complete the exercise as indicated on the handout for assignment #5
-// 2. Then delete these comments
-// 3. Finally, commit and push the project containing this file to GitHub
-
-// -------RUBRIC-------
-// Functionality								/6
-// Methods adhere to program requirements	/5
-// Value returned from calculatePremium()	/3
-// Coding conventions & comments				/6
-
+import javax.swing.JOptionPane;
 
 public class InsurancePremium {
 
 	public static void main(String[] args) {
-		//Insert body of main() method here
+		//Declare variables
+		int curYear, birthYear, prem;
+		
+		//User input
+		curYear = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the current year:"));
+		birthYear = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter your birth year:"));
+		
+		prem = calculatePremium(curYear, birthYear);
+		
+		JOptionPane.showMessageDialog(null, "Your insurance premium is $" + prem);
+					
 	}
 	
-	public static int calculatePremium(/*Insert parameters here*/) {
-		//Insert body of calculatePremium() method here
+	public static int calculatePremium(int cy, int by) {
+		//Declare variables
+		int age = cy - by;
+		int decades = age / 10;
+		int premium = (decades + 15) * 20;
+		return premium;
 	}
 
 }
